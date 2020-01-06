116ab3358ac429009c0d20be8557b75a6471aa9e
LuisOToro <lotoro@espol.edu.ec>
2020-01-06 08:55:32 -0500

    Unido_Menu

---
+++ Assets/Scripts/enemyFrog.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyFrog : MonoBehaviour
+{
+    public float speed = 5;
+    public int direction = 1;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(2 * Time.deltaTime * speed * direction, 0, 0);
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("limit"))
+        {
+            direction *= -1;
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+}

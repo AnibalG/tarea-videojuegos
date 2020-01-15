9e23ab54568e87a178115b35930cc3d520f91ffa
ceva94 <ceva94@hotmail.com>
2020-01-14 23:51:52 -0500

    intro y creditos

---
+++ Assets/Scripts/videoIntro.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class videoIntro : MonoBehaviour
+{
+    public Camera camara;
+    public VideoPlayer video;
+    public float tiempo;
+    public float tiempoAnimacion;
+    public GameObject pantalla;
+    public GameObject v;
+    private void Awake()
+    {
+        camara = GetComponent<Camera>();
+        video = GetComponent<VideoPlayer>();
+
+
+    }
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempo += Time.deltaTime;
+        if (tiempo > tiempoAnimacion) {
+            pantalla.SetActive(true);
+            v.SetActive(false);
+
+        }
+        
+        
+    }
+}
